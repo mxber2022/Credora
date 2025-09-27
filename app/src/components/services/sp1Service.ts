56fb6fe842b7663b96216c5d6ce0754302e2d7e7
MX <mxber2022@gmail.com>
2025-09-27 13:51:49 +0530

    services

---
+++ app/src/components/services/sp1Service.ts
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+const API_URL = 'https://relayer-api.horizenlabs.io/api/v1';
+
+export const verifyProofWithRelayer = async (latestProof: any, setStatus?: (status: string) => void): Promise<{txHash: string, aggregationDetails: any, aggregationId: number}> => {
+  console.log([
+    latestProof?.proof.pubkeyHash, 
+    latestProof?.proof.nullifier, 
+    latestProof?.proof.timestamp, 
+    latestProof?.proof.ageAbove18, 
+    latestProof?.proof.gender, 
+    latestProof?.proof.pincode, 
+    latestProof?.proof.state, 
+    latestProof?.proof.nullifierSeed, 
+    latestProof?.proof.signalHash
+  ]);
+
+  const params = {
+    "proofType": "groth16",
+    "vkRegistered": true,
+    "chainId": 845320009,
+    "proofOptions": {
+      "library": "snarkjs",
+      "curve": "bn128"
+    },
+    "proofData": {
+      "proof": latestProof?.proof.groth16Proof,
+      "publicSignals": [
+        latestProof?.proof.pubkeyHash, 
+        latestProof?.proof.nullifier, 
+        latestProof?.proof.timestamp, 
+        latestProof?.proof.ageAbove18, 
+        latestProof?.proof.gender, 
+        latestProof?.proof.pincode, 
+        latestProof?.proof.state, 
+        latestProof?.proof.nullifierSeed, 
+        latestProof?.proof.signalHash
+      ],
+      "vk": "0x0b692be7b498a34664f07464866c2948b3ba925657185e5f2323be452bfd6722"
+    }
+  };
+
+  setStatus?.('Submitting proof to relayer...');
+  
+  const requestResponse = await axios.post(
+    // @ts-ignore
+    `${API_URL}/submit-proof/${import.meta.env.VITE_API_KEY}`, 
+    params
+  );
+
+  setStatus?.('Proof submitted, waiting for aggregation...');
+  while (true) {
+    const jobStatusResponse = await axios.get(
+          // @ts-ignore
+      `${API_URL}/job-status/${import.meta.env.VITE_API_KEY}/${requestResponse.data.jobId}`
+    );
+    
+    const currentStatus = jobStatusResponse.data.status;
+    setStatus?.(`Job status: ${currentStatus}`);
+    
+    if (currentStatus === "Aggregated") {
+      console.log("Job finalized successfully");
+      console.log(jobStatusResponse.data);
+      const txHash = `https://zkverify-testnet.subscan.io/extrinsic/${jobStatusResponse.data.txHash}`;
+      return {
+        txHash,
+        aggregationDetails: jobStatusResponse.data.aggregationDetails,
+        aggregationId: jobStatusResponse.data.aggregationId
+      };
+    } else {
+      console.log("Job status: ", currentStatus);
+      console.log("Waiting for job to finalize...");
+      await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for 5 seconds before checking again
+    }
+  }
+}; 
